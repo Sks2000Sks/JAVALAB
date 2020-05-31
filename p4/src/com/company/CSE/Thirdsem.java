6a38a16e160df3445dad63db54a65048ac8d9436
sksvk2000 <52601700+sksvk2000@users.noreply.github.com>
2020-05-31 11:49:10 +0530

    Add files via upload

---
+++ p4/src/com/company/CSE/Thirdsem.java
@@ -0,0 +1,7 @@
+package com.company.CSE;
+
+public class Thirdsem {
+ public void Welcomemsg(){
+        System.out.println("Welcome bla bla bla");
+    }
+}
---
+++ p4/src/com/company/RVCE/Cse_dept.java
@@ -0,0 +1,16 @@
+package com.company.RVCE;
+
+import com.company.CSE.Thirdsem;
+
+public class Cse_dept extends Thirdsem {
+    @Override
+    public void Welcomemsg()
+    {
+        super.Welcomemsg();
+    }
+
+    public static void main(String[] args) {
+        Cse_dept c=new Cse_dept();
+        c.Welcomemsg();
+    }
+}
